57a1bbf1e2049e3a9f9ceedb1022112d82489cf4
Manoj <manuvini29@gmail.com>
2023-08-24 16:42:07 +0530

    header footer

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,21 +1,123 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <header className="bg-black py-4">
-      <nav className="flex items-center justify-between max-w-6xl mx-auto px-4">
-        <div className="flex items-center space-x-2">
-          {/* <img src="https://example.com/path-to-your-image.png" alt="Logo" className="h-8 w-8" /> Add your online logo */}
-          <a href="#" className="text-gray-400 text-xl">Clout services</a>
-        </div>
-        <ul className="flex space-x-4">
-          <li><a href="#" className="text-white hover:text-blue-300">Home</a></li>
-          <li><a href="#" className="text-white hover:text-blue-300">About</a></li>
-          <li><a href="#" className="text-white hover:text-blue-300">Services</a></li>
-          <li><a href="#" className="text-white hover:text-blue-300">Contact</a></li>
+    <>
+      <header className="bg-black py-4">
+        <nav className="flex items-center justify-between max-w-6xl mx-auto px-4">
+          <div className="flex items-center space-x-2">
+            {/* Your logo */}
+            <a href="#" className="text-white text-2xl pl-5">
+              Clout services
+            </a>
+          </div>
+          <div className="lg:hidden">
+            <button
+              onClick={toggleMenu}
+              className="text-white text-2xl focus:outline-none"
+            >
+              {/* Hamburger menu icon */}
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            </button>
+          </div>
+          <ul className="hidden lg:flex space-x-10">
+            <li>
+              <a
+                href="#"
+                className="text-white hover:text-blue-300"
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="text-white hover:text-blue-300"
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="text-white hover:text-blue-300"
+              >
+                Services
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="text-white hover:text-blue-300"
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <div
+        className={`w-full lg:hidden ${isMenuOpen ? 'block' : 'hidden'}`}
+        style={{
+          position: 'absolute',
+          zIndex: 999,
+        }}
+      >       <ul className="bg-black text-white flex-col py-2 space-y-1 lg:space-y-0 lg:flex-row lg:space-x-6">
+          <li>
+            <a
+              href="#"
+              className="text-white hover:text-blue-300 block lg:inline-block lg:text-white p-2 px-6 border-b border-gray-700"
+            >
+              Home
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="text-white hover:text-blue-300 block lg:inline-block lg:text-white p-2 px-6 border-b border-gray-700"
+            >
+              About
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="text-white hover:text-blue-300 block lg:inline-block lg:text-white p-2 px-6 border-b border-gray-700"
+            >
+              Services
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="text-white hover:text-blue-300 block lg:inline-block lg:text-white p-2 px-6 border-b border-gray-700"
+            >
+              Contact
+            </a>
+          </li>
         </ul>
-      </nav>
-    </header>
+      </div>
+    </>
   );
 }
 
