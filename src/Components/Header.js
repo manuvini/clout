495f2126a99a984bb33a2cb23aeff3f5d7cc9e2d
Prashanth <prashanthpkjb8@gmail.com>
2023-08-24 15:38:51 +0530

    header bro

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,22 @@
-const Header = ( ) => {
+import React from 'react';
 
-    return(
-
-        <>
-        
-        </>
-    )
+function Header() {
+  return (
+    <header className="bg-black py-4">
+      <nav className="flex items-center justify-between max-w-6xl mx-auto px-4">
+        <div className="flex items-center space-x-2">
+          {/* <img src="https://example.com/path-to-your-image.png" alt="Logo" className="h-8 w-8" /> Add your online logo */}
+          <a href="#" className="text-gray-400 text-xl">Clout services</a>
+        </div>
+        <ul className="flex space-x-4">
+          <li><a href="#" className="text-white hover:text-blue-300">Home</a></li>
+          <li><a href="#" className="text-white hover:text-blue-300">About</a></li>
+          <li><a href="#" className="text-white hover:text-blue-300">Services</a></li>
+          <li><a href="#" className="text-white hover:text-blue-300">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
